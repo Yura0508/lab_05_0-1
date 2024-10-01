3cad24008e3dcfdd01bbc4f5ace3af204be1e77f
Yura <urijkisil2007@gmail.com>
2024-10-01 22:24:37 +0300

    first testbranch commit

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,30 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab_5_1/lab_5_1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			
+
+			//double k = (f(1, 1, 1) + f(1, 1, 1)) / (1 + pow(f(1, 1, 1), 2));
+			//Assert::AreEqual(k, 0.44714);
+                
+			double tolerance = 0.000001;
+			double k = (f(1, 1, 1) + f(1, 1, 1)) / (1 + pow(f(1, 1, 1), 2));
+			double expectedValue = 0.44714;
+
+			Assert::IsTrue(abs(k - expectedValue) < tolerance);
+
+
+
+		}
+	};
+}
